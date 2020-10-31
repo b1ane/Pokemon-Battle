a6ab7ba32fdbad5ec5dc461c023914818fc97350
Blane Zewdie <b1ane@outlook.com>
2020-10-31 17:50:53 -0500

    Initial Commit

---
+++ Pokemon-Battle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pokemon-Battle
+//
+//  Created by Blane Zewdie on 10/31/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
